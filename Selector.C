a538cc96236c298f63986c1369acb41247d9a89d
SystemOfLife <sayers_111@mail.ru>
2018-05-12 22:36:43 +0300

    Adding Selector

---
+++ Selector.C
@@ -0,0 +1,196 @@
+#define Selector_cxx
+// The class definition in Selector.h has been generated automatically
+// by the ROOT utility TTree::MakeSelector(). This class is derived
+// from the ROOT class TSelector. For more information on the TSelector
+// framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
+
+// The following methods are defined in this file:
+//    Begin():        called every time a loop on the tree starts,
+//                    a convenient place to create your histograms.
+//    SlaveBegin():   called after Begin(), when on PROOF called only on the
+//                    slave servers.
+//    Process():      called for each event, in this function you decide what
+//                    to read and fill your histograms.
+//    SlaveTerminate: called at the end of the loop on the tree, when on PROOF
+//                    called only on the slave servers.
+//    Terminate():    called at the end of the loop on the tree,
+//                    a convenient place to draw/fit your histograms.
+//
+// To use this file, try the following session on your Tree T:
+//
+// Root > T->Process("Selector.C")
+// Root > T->Process("Selector.C","some options")
+// Root > T->Process("Selector.C+")
+//
+
+#include "Selector.h"
+#include <TH2.h>
+#include <TH1.h>
+#include <TStyle.h>
+#include <TMath.h>
+const Double_t c2 = TMath::C() * TMath::C();
+#define phi_mass2 1039298.6916
+#define sigma_m2 0.016
+//#define sigma
+
+
+void Selector::Begin(TTree * /*tree*/)
+{
+   // The Begin() function is called at the start of the query.
+   // When running with PROOF Begin() is only called on the client.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+   f = new TFile("testfile.root", "recreate");
+
+
+   hm2pE = new TH2F ("hm2pE","m^{2}", 200,-0.2,1.5,200, -100,100); //m^{2} vs p histogram booking
+   hm2pW = new TH2F ("hm2pW", "m^{2}", 200,-0.2,1.5,200, -100,100); //m^{2} vs p histogram booking
+   minv = new TH1F ("minv","Invariant mass",200,-0.2,100);
+
+
+   TString option = GetOption();
+
+}
+
+void Selector::SlaveBegin(TTree * /*tree*/)
+{
+   // The SlaveBegin() function is called after the Begin() function.
+   // When running with PROOF SlaveBegin() is called on each slave server.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+
+}
+
+Bool_t Selector::Process(Long64_t entry)
+{
+   // The Process() function is called for each entry in the tree (or possibly
+   // keyed object in the case of PROOF) to be processed. The entry argument
+   // specifies which entry in the currently loaded tree is to be processed.
+   // It can be passed to either Selector::GetEntry() or TBranch::GetEntry()
+   // to read either all or the required parts of the data. When processing
+   // keyed objects with PROOF, the object is already loaded and is available
+   // via the fObject pointer.
+   //
+   // This function should contain the "body" of the analysis. It can contain
+   // simple or elaborate selection criteria, run algorithms on the data
+   // of the event and typically fill histograms.
+   //
+   // The processing can be stopped by calling Abort().
+   //
+   // Use fStatus to set the return value of TTree::Process().
+   //
+   // The return value is currently not used.
+
+
+    if (entry % 100000 == 0)
+    {
+         std::cout << entry << std::endl;
+    }
+
+/*    if (entry > 1000)
+    {
+      Abort("BFVSDGFVD");
+    }*/
+
+   fChain->GetEntry(entry); 
+
+
+   if(cent>0 && cent<=80){
+        // std::cout << "cenOk" << std::endl;
+      /*hcent->Fill(centrality);
+      hRun->Fill(run);
+      hbbcz->Fill(bbcz);*/
+      for(int itrk=0;itrk<mh;itrk++) {  //track loop
+         //if(pt>0.4&&pt<3.8){ // track selection
+         std::cout << "dcarm[itrk]: " << dcarm[itrk] << "    pltof[itrk]: " << pltof[itrk]<< "    etof[itrk]: " << etof[itrk]<< "    sigtof[itrk]: " << sigtof[itrk] << std::endl;
+            if((dcarm[itrk]==0) && (pltof[itrk]>0) && (etof[itrk]>0.002) && (sigtof[itrk]<3){        //Tof East selection
+               std::cout << "hm2pEOk" << std::endl;
+               float tmp=(ttof[itrk]*c2)/pltof[itrk];
+               m2=(p[itrk]*p[itrk]*((tmp*tmp)-1))/c2;
+               m2sigma=(abs(m2-phi_mass2))/sigma_m2;
+               hm2pE->Fill(m2sigma,p[itrk]);
+            }
+
+            if( m2sigma > (phi_mass2-sigma_m2) && m2sigma < (phi_mass2 + sigma_m2) && (charge[itrk] > 0)){
+               par_1.SetRho(p[itrk]);
+               par_1.SetTheta(the0[itrk]);
+               par_1.SetPhi(phi0[itrk]);
+               par_1.SetE(etof[itrk]);
+               for( Int_t jtrk=0; jtrk<mh; jtrk++){
+                  if (m2sigma > (phi_mass2-sigma_m2) && (m2sigma<phi_mass2+sigma_m2) && (charge[itrk]<0)){
+                     par_2.SetRho(p[jtrk]);
+                     par_2.SetTheta(the0[jtrk]);
+                     par_2.SetE(etof[jtrk]);
+                     par_2.SetPhi(phi0[jtrk])
+                  }
+                  pair=par_1+par_2;
+                  m_inv=pair.M();
+                  minv->Fill(m_inv);
+               
+                  
+               }
+            }// end of tof east selection
+
+
+
+
+
+               if((dcarm[itrk]==1) && (pltof[itrk]>0) && (etof[itrk]>60) && (etof[itrk]<600) && (sigtof[itrk]<3)){// Tof West selection
+                  float tmp=(ttof[itrk]*c2)/pltof[itrk];
+                  m2=(p[itrk]*p[itrk]*((tmp*tmp)-1))/c2;
+                  m2sigma=(abs(m2-phi_mass2))/sigma_m2;
+                  hm2pW->Fill(m2,p[itrk]);
+                  if(m2sigma>phi_mass2-sigma_m2 && m2sigma<phi_mass2+sigma_m2 && charge[itrk]>0){
+                     par_1.SetRho(p[itrk]);
+                     par_1.SetTheta(the0[itrk]);
+                     par_1.SetPhi(phi0[itrk]);
+                     par_1.SetE(etof[itrk]);
+                     for( Int_t jtrk=0; jtrk<mh;jtrk++){
+                        if (m2sigma>phi_mass2-sigma_m2 && m2sigma<phi_mass2+sigma_m2 && charge[itrk]<0){
+                           par_2.SetRho(p[jtrk]);
+                           par_2.SetTheta(the0[jtrk]);
+                           par_2.SetE(etof[jtrk]);
+                           par_2.SetPhi(phi0[jtrk])
+                        }
+                     pair=par_1+par_2;
+                     m_inv=pair.M();
+                     minv->Fill(m_inv);
+                  
+                     
+                     }
+                  }
+               }
+   return kTRUE;
+      }
+   }
+
+         
+
+
+
+void Selector::SlaveTerminate()
+{
+   // The SlaveTerminate() function is called after all entries or objects
+   // have been processed. When running with PROOF SlaveTerminate() is called
+   // on each slave server.
+   std::cout << "!!!DEBUG111" << std::endl;
+   hm2pE->Write();
+   hm2pW->Write();
+   minv->Write();
+   std::cout << "!!!DEBUG222" << std::endl;
+
+   //delete hm2pE;
+   //delete hm2pW;
+   //delete minv;
+   std::cout << "!!!DEBUG333" << std::endl;
+
+   f->Close();
+   std::cout << "!!!DEBUG444" << std::endl;
+}
+
+void Selector::Terminate()
+{
+   // The Terminate() function is the last function to be called during
+   // a query. It always runs on the client, it can be used to present
+   // the results graphically or save the results to file.
+}
---
+++ Selector.h
@@ -0,0 +1,218 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Thu Apr 26 19:07:19 2018 by ROOT version 5.34/30
+// from TTree mtree/Hadron EMC + TOF tree
+// found on file: /home/arkady/NIRS/RootFiles/new62/311857.root
+//////////////////////////////////////////////////////////
+
+#ifndef Selector_h
+#define Selector_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+
+// Header file for the classes stored in the TTree if any.
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class Selector : public TSelector {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+
+   // Declaration of leaf types
+   Int_t           run;
+   Float_t         bbcz;
+   Float_t         cent;
+   Int_t           rh;
+   Float_t         phir[47];   //[rh]
+   Float_t         time[47];   //[rh]
+   Float_t         qr0[47];   //[rh]
+   Float_t         etar[47];   //[rh]
+   Short_t         armr[47];   //[rh]
+   Short_t         ring[47];   //[rh]
+   Int_t           chid[47];   //[rh]
+   Int_t           mh;
+   Float_t         alpha[52];   //[mh]
+   Short_t         dcarm[52];   //[mh]
+   Float_t         p[52];   //[mh]
+   Short_t         charge[52];   //[mh]
+   Float_t         phi0[52];   //[mh]
+   Float_t         the0[52];   //[mh]
+   Float_t         phi[52];   //[mh]
+   Float_t         ecore[52];   //[mh]
+   Float_t         plemc[52];   //[mh]
+   Float_t         ecent[52];   //[mh]
+   Float_t         temc[52];   //[mh]
+   Float_t         temcpi[52];   //[mh]
+   Float_t         temcp[52];   //[mh]
+   Float_t         temck[52];   //[mh]
+   Short_t         sect[52];   //[mh]
+   Float_t         isPiemc[52];   //[mh]
+   Float_t         isPemc[52];   //[mh]
+   Float_t         isKemc[52];   //[mh]
+   Int_t           idtwr[52];   //[mh]
+   Float_t         sigtof[52];   //[mh]
+   Float_t         sigpc3[52];   //[mh]
+   Float_t         sigemc[52];   //[mh]
+   Float_t         res[52];   //[mh]
+   Float_t         ttof[52];   //[mh]
+   Int_t           slat[52];   //[mh]
+   Float_t         pltof[52];   //[mh]
+   Float_t         etof[52];   //[mh]
+   Float_t         isPi[52];   //[mh]
+   Float_t         isP[52];   //[mh]
+   Float_t         isK[52];   //[mh]
+
+   // List of branches
+   TBranch        *b_run;   //!
+   TBranch        *b_bbcz;   //!
+   TBranch        *b_cent;   //!
+   TBranch        *b_rh;   //!
+   TBranch        *b_phir;   //!
+   TBranch        *b_time;   //!
+   TBranch        *b_qr0;   //!
+   TBranch        *b_etar;   //!
+   TBranch        *b_armr;   //!
+   TBranch        *b_ring;   //!
+   TBranch        *b_chid;   //!
+   TBranch        *b_mh;   //!
+   TBranch        *b_alpha;   //!
+   TBranch        *b_dcarm;   //!
+   TBranch        *b_p;   //!
+   TBranch        *b_charge;   //!
+   TBranch        *b_phi0;   //!
+   TBranch        *b_the0;   //!
+   TBranch        *b_phi;   //!
+   TBranch        *b_ecore;   //!
+   TBranch        *b_plemc;   //!
+   TBranch        *b_ecent;   //!
+   TBranch        *b_temc;   //!
+   TBranch        *b_temcpi;   //!
+   TBranch        *b_temcp;   //!
+   TBranch        *b_temck;   //!
+   TBranch        *b_sect;   //!
+   TBranch        *b_isPiemc;   //!
+   TBranch        *b_isPemc;   //!
+   TBranch        *b_isKemc;   //!
+   TBranch        *b_idtwr;   //!
+   TBranch        *b_sigtof;   //!
+   TBranch        *b_sigpc3;   //!
+   TBranch        *b_sigemc;   //!
+   TBranch        *b_res;   //!
+   TBranch        *b_ttof;   //!
+   TBranch        *b_slat;   //!
+   TBranch        *b_pltof;   //!
+   TBranch        *b_etof;   //!
+   TBranch        *b_isPi;   //!
+   TBranch        *b_isP;   //!
+   TBranch        *b_isK;   //!
+
+   TLorentzVector par_1;  //Initialisation of var for Selector.C
+   TLorentzVector par_2;
+   TLorentzVector pair;
+   TH2F *hm2pE;
+   TH2F *hm2pW;
+   TH1F *minv;
+   TFile *f;
+
+   Float_t m2, m2sigma;
+   Float_t sigtof;
+
+   Selector(TTree * /*tree*/ =0) : fChain(0) { }
+   virtual ~Selector() { }
+   virtual Int_t   Version() const { return 2; }
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual void    Init(TTree *tree);
+   virtual Bool_t  Notify();
+   virtual Bool_t  Process(Long64_t entry);
+   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+   virtual void    SetOption(const char *option) { fOption = option; }
+   virtual void    SetObject(TObject *obj) { fObject = obj; }
+   virtual void    SetInputList(TList *input) { fInput = input; }
+   virtual TList  *GetOutputList() const { return fOutput; }
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(Selector,0);
+};
+
+#endif
+
+#ifdef Selector_cxx
+void Selector::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("run", &run, &b_run);
+   fChain->SetBranchAddress("bbcz", &bbcz, &b_bbcz);
+   fChain->SetBranchAddress("cent", &cent, &b_cent);
+   fChain->SetBranchAddress("rh", &rh, &b_rh);
+   fChain->SetBranchAddress("phir", phir, &b_phir);
+   fChain->SetBranchAddress("time", time, &b_time);
+   fChain->SetBranchAddress("qr0", qr0, &b_qr0);
+   fChain->SetBranchAddress("etar", etar, &b_etar);
+   fChain->SetBranchAddress("armr", armr, &b_armr);
+   fChain->SetBranchAddress("ring", ring, &b_ring);
+   fChain->SetBranchAddress("chid", chid, &b_chid);
+   fChain->SetBranchAddress("mh", &mh, &b_mh);
+   fChain->SetBranchAddress("alpha", alpha, &b_alpha);
+   fChain->SetBranchAddress("dcarm", dcarm, &b_dcarm);
+   fChain->SetBranchAddress("p", p, &b_p);
+   fChain->SetBranchAddress("charge", charge, &b_charge);
+   fChain->SetBranchAddress("phi0", phi0, &b_phi0);
+   fChain->SetBranchAddress("the0", the0, &b_the0);
+   fChain->SetBranchAddress("phi", phi, &b_phi);
+   fChain->SetBranchAddress("ecore", ecore, &b_ecore);
+   fChain->SetBranchAddress("plemc", plemc, &b_plemc);
+   fChain->SetBranchAddress("ecent", ecent, &b_ecent);
+   fChain->SetBranchAddress("temc", temc, &b_temc);
+   fChain->SetBranchAddress("temcpi", temcpi, &b_temcpi);
+   fChain->SetBranchAddress("temcp", temcp, &b_temcp);
+   fChain->SetBranchAddress("temck", temck, &b_temck);
+   fChain->SetBranchAddress("sect", sect, &b_sect);
+   fChain->SetBranchAddress("isPiemc", isPiemc, &b_isPiemc);
+   fChain->SetBranchAddress("isPemc", isPemc, &b_isPemc);
+   fChain->SetBranchAddress("isKemc", isKemc, &b_isKemc);
+   fChain->SetBranchAddress("idtwr", idtwr, &b_idtwr);
+   fChain->SetBranchAddress("sigtof", sigtof, &b_sigtof);
+   fChain->SetBranchAddress("sigpc3", sigpc3, &b_sigpc3);
+   fChain->SetBranchAddress("sigemc", sigemc, &b_sigemc);
+   fChain->SetBranchAddress("res", res, &b_res);
+   fChain->SetBranchAddress("ttof", ttof, &b_ttof);
+   fChain->SetBranchAddress("slat", slat, &b_slat);
+   fChain->SetBranchAddress("pltof", pltof, &b_pltof);
+   fChain->SetBranchAddress("etof", etof, &b_etof);
+   fChain->SetBranchAddress("isPi", isPi, &b_isPi);
+   fChain->SetBranchAddress("isP", isP, &b_isP);
+   fChain->SetBranchAddress("isK", isK, &b_isK);
+
+   std::cout << fChain->GetEntries() << " entries" << std::endl;
+
+}
+
+Bool_t Selector::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+#endif // #ifdef Selector_cxx
